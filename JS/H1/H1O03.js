d6cad7839fbac3e14c8a603cd7c3a24b9fbe1921
152919 <152919@csgaugustinus.nl>
2021-11-24 15:07:08 +0000

    opdr 1t/m 3

--- JS/H1/H1O03.js
+++ JS/H1/H1O03.js
@@ -3,16 +3,17 @@ function setup() {
   background('white');
   canvas.parent('processing');
   noLoop();
-  // colorMode(RGB,255,255,255,1);  
+  colorMode(RGB,255,255,255,1);  
 }
 
 function draw() {
   noStroke();
-  // fill(0,0,255,0.3);
-  fill('red');
+  fill(0,0,255,0.3);
   rect(0,150,450,150);
-  fill('green');
   rect(0,0,150,450);
-  fill('blue');  
   rect(75,0,375,450);
+  circle(450,225,450);
+  fill(0,225,0,0.3);
+  triangle(225,0,225,450,450,225);
+  // the red block is not visible because it is coverd by the other blocks
 }
